0738c3576e8d00b80eb8be523847a661960e43c0
amogh <amogh.johri@iiitb.org>
2020-10-11 06:55:08 +0530

    -

--- HCP_Large/script.py
+++ HCP_Large/script.py
@@ -4,7 +4,7 @@ import subprocess
 from_ = sys.argv[1]
 
 sudo_password = 'password'
-command = ("docker run -v /path/to/HCP_YA_BIDS:/path/to/HCP_YA_BIDS -v /path/to/output_legacy:/Final_Output_Legacy -v /path/to/C_Input:/C_Input amoghjohri/tvbpipeline_aws python pipeline.py legacy 1 LR " + str(buckets[rank][0]) + " " + str(buckets[rank][1]) + " /path/to/HCP_YA_BIDS 0 61 4")
+command = ("docker run -v /path/to/HCP_YA_BIDS:/path/to/HCP_YA_BIDS -v /path/to/output_legacy:/Final_Output_Legacy -v /path/to/C_Input:/C_Input amoghjohri/tvbpipeline_aws python pipeline.py legacy 1 LR " + from_ + " " + str(int(from_) + 1) + " /path/to/HCP_YA_BIDS 0 61 4")
 command       = command.split()
 
 cmd1   = subprocess.Popen(['echo',sudo_password], stdout=subprocess.PIPE)
